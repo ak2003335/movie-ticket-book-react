1f71097b3ac5a8f4c511ac0033027901b4025da3
Priya Prakash <priyprakashpandey@gmail.com>
2023-05-27 17:19:01 +0530

    Home Page

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="text-center">
+      <h1 style={{ fontSize: "60px", marginTop: "10vw" }}>
+        <b>
+          BOOK YOUR <br /> TICKETS FOR{" "}
+          <span style={{ color: "green" }}>MOVIES</span>
+        </b>
+      </h1>
+      <h5>
+        Safe, secure, reliable ticketing. Your ticket to live entertainment
+      </h5>
+    </div>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark">
+      <div class="container-fluid">
+        <Link class="navbar-brand d-flex" to={"/"}>
+          <img
+            src="https://www.shutterstock.com/shutterstock/videos/1100748499/thumb/12.jpg?ip=x480"
+            alt=""
+            height={60}
+          />
+          <h2 style={{ marginLeft: "-25px", marginTop: "10px" }}>
+            <b>
+              Movie<span className="clr">Tic</span>{" "}
+            </b>
+          </h2>
+        </Link>
+        <div className="float-right">
+            <Link className="custBtn px-4" to={'/signin'}><b>SIGN IN</b></Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
